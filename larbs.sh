cc81694e00da4c6a0f82060d6be2d74254cc7b33
Eman2122 <118656812+Eman2122@users.noreply.github.com>
2022-11-20 09:19:12 -0500

    Update larbs.sh

--- larbs.sh
+++ larbs.sh
@@ -7,7 +7,7 @@
 ### OPTIONS AND VARIABLES ###
 
 dotfilesrepo="https://github.com/lukesmithxyz/voidrice.git"
-progsfile="https://raw.githubusercontent.com/LukeSmithxyz/LARBS/master/progs.csv"
+progsfile="https://github.com/Eman2122/LARBS/blob/master/progs.csv"
 aurhelper="yay"
 repobranch="master"
 
@@ -25,7 +25,7 @@ error() {
 
 welcomemsg() {
 	whiptail --title "Welcome!" \
-		--msgbox "Welcome to Luke's Auto-Rice Bootstrapping Script!\\n\\nThis script will automatically install a fully-featured Linux desktop, which I use as my main machine.\\n\\n-Luke" 10 60
+		--msgbox "Welcome to Eman2122 Auto-Rice Bootstrapping Script!\\n\\nThis script will automatically install a fully-featured Linux desktop, which I use as my main machine.\\n\\n-Luke" 10 60
 
 	whiptail --title "Important Note!" --yes-button "All ready!" \
 		--no-button "Return..." \
